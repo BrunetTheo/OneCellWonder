dfed55342cf0f3cbc6ce07bbe5d810876da0e183
mstavras <mael.rm.stavras@gmail.com>
2026-01-29 15:57:56 +0100

    test with game of life, start/stop, forward/backward, Iteration counter

--- interface.py
+++ interface.py
@@ -4,6 +4,19 @@ import math
 
 windows_size = (800, 800)
 
+def step(grid: np.ndarray) -> np.ndarray:
+    neighbors = sum(
+        np.roll(np.roll(grid, dy, axis=0), dx, axis=1)
+        for dy in (-1, 0, 1)
+        for dx in (-1, 0, 1)
+        if not (dx == 0 and dy == 0)
+    )
+
+    birth = (neighbors == 3) & (grid == 0) # Rule 1
+    survive = ((neighbors == 2) | (neighbors == 3)) & (grid == 1) # Rule 2
+    
+    return (birth | survive).astype(np.uint8)
+
 def polygon_points(center, size):
     height = size
     width = math.sqrt(3) * size
@@ -21,9 +34,13 @@ def main():
     screen = pygame.display.set_mode(windows_size)
     pygame.display.set_caption("my beautiful window that will hopefully work one day")
     running = False
-    matrix = np.random.randint(0,2,size=(50, 50))
+    matrix = np.random.randint(0,2,size=(500, 500))
     color = [(0,0,0), (255, 255, 255)]
     cell_size = (windows_size[0] - 50) / (3.5 * matrix.shape[0])
+    clock = pygame.time.Clock()
+    iteration_counter = 0
+    matrix_history = []
+    font = pygame.font.Font('freesansbold.ttf', 24)
 
     def draw_polygon(center, color):
         pygame.draw.polygon(
@@ -58,7 +75,12 @@ def main():
                     draw_polygon([center[0], center[1]], color[matrix[i, j]])
             center[0] = initial_center[0]
             center[1] = i * 2 * width + initial_center[1]
+        text = font.render(f'Iteration {iteration_counter}', True, (255,255,255))
+        textRect = text.get_rect()
+        textRect.bottomright = windows_size
+        screen.blit(text, textRect)
         pygame.display.flip()
+    matrix_history.append(matrix)
 
     # Main loop
     while True:
@@ -66,8 +88,26 @@ def main():
             if event.type == pygame.QUIT: #close
                 pygame.quit()
                 return
-        draw_grid()
 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE: # Start/ stop
+                    running = not running
+                if event.key == pygame.K_RIGHT:
+                    matrix = step(matrix)
+                    iteration_counter += 1
+                    matrix_history.append(matrix)
+                if event.key == pygame.K_LEFT:
+                    iteration_counter -= 1
+                    matrix = matrix_history[iteration_counter]
+                    matrix_history = matrix_history[:-1]
+
+        
+        if running:
+            matrix = step(matrix)
+            iteration_counter += 1
+            matrix_history.append(matrix)
+        
+        draw_grid()
 
 if __name__ == "__main__":
     main()
