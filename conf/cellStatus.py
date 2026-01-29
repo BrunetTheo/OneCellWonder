5608733ada964ab7535853bbd5683ed3da2283fc
Arbona <jeanmichel.arbona@ens-lyon.fr>
2026-01-29 16:38:06 +0100

    Implemented validate_rule

--- conf/cellStatus.py
+++ conf/cellStatus.py
@@ -6,7 +6,7 @@ class CellGrid:
         C: Number of Cells
         G: Number of genes
         alive_grid: np.array [grid dimension]
-        gene_grid: np.array [G x C]
+        gene_grid: np.array [W x L x G]
         """
         self.alive_grid = alive_grid
         self.gene_grid = gene_grid
@@ -18,10 +18,43 @@ class CellGrid:
     def get_genes(self, cell_indices=None):
         """Returns set of active genes per cell"""
         pass
-       
+    
+    def validate_rule(self,positive_genes,negative_genes,n_neighboor=None):
+        """ Check If a rule return true
+        
+        positive_genes = numpy array or gene that must be present e.g [1] when the second gene need to be there
+        negatvie_genes = numpy array or gene that must be absent e.g [2,3] when the third and fourth gene must be absent
 
-    def match_rule(self, rule):
+        """
+
+        n_positive = len(positive_genes)
+        if n_positive != 0:
+            positive_gene_validation = np.sum(self.gene_grid[:,:,positive_genes],axis=-1) == n_positive
+        else:
+            positive_gene_validation = np.ones_like(self.gene_grid[:,:,0],dtype=bool)
+
+        n_negative = len(negative_genes)
+        if n_negative != 0:
+
+            negative_gene_validation = np.sum(1-self.gene_grid[:,:,negative_genes],axis=-1) == n_negative
+        else:
+            negative_gene_validation = np.ones_like(self.gene_grid[:,:,0],dtype=bool)
+
+
+        gene_validation = positive_gene_validation * negative_gene_validation
+
+        if n_neighboor != None:
+            gene_validation = gene_validation * (n_neighboor == self.get_n_neighboor())
+        
+        return gene_validation
+
+
+    def match_rules(self, rules):
         """matches the rules to cell status """
+        for rule in rules
         pass
     def update_grid(self):
         pass
+
+
+
