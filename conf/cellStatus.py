a82e4e3da2053d01d1e6dc2e5db020c27ce7b683
Maelle Spiller <mspiller@uni-potsdam.de>
2026-01-29 17:36:17 +0100

    start of neighborhoodmask

--- conf/cellStatus.py
+++ conf/cellStatus.py
@@ -1,20 +1,38 @@
 import numpy as np
 
 class CellGrid:
-    def __init__(self, alive_grid, gene_grid):
+    def __init__(self, state_grid, gene_grid):
         """
         C: Number of Cells
         G: Number of genes
-        alive_grid: np.array [grid dimension]
+        state_grid: np.array [grid dimension]
         gene_grid: np.array [G x C]
         """
-        self.alive_grid = alive_grid
+        self.state_grid = state_grid
         self.gene_grid = gene_grid
 
     def get_coords(self):
         """Return coordinates of all cells on the grid. Implies whether a cell is dead or alive"""
         pass
-       
+    def get_neighbors(self):
+        pass 
+    
+    
+    def apply_neighborhoodmask(self, gene_grid, rule_applied_grid, affected_neighborhood, gene_idx):
+        """based on the affected_neighborhood, it creates a mask of size affected_neighborhood, 
+        and adds applies the mask to the origin of signal, which is indicated in rule_applied_grid rule_applied_grid: 
+        matrix of coordinates that tells you where signal origin lies"""
+        rows, cols = gene_grid
+        mask = np.zeros(gene_grid, dtype= bool)
+
+        # use a mask per cell of origin
+            
+            
+
+    
+
+
+    
     def get_genes(self, cell_indices=None):
         """Returns set of active genes per cell"""
         pass
@@ -23,5 +41,14 @@ class CellGrid:
     def match_rule(self, rule):
         """matches the rules to cell status """
         pass
+    
+    
     def update_grid(self):
         pass
+    
+    
+
+
+
+
+# 
